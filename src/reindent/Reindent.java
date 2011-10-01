3bb1ec005550490601cf2253d6f5ed61b6f3ff19
Sean Fisk <sean@seanfisk.com>
2011-09-30 20:09:39 -0400

    Created reindent source file, add .gitignore.

---
+++ src/reindent/Reindent.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package reindent;
+
+/**
+ * @author Taylor Countryman <countryman.taylor@gmail.com>
+ */
+public class Reindent
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		if(args.length != 2)
+		{
+			System.err.println("Usage: java reindent.Reindent INFILE OUTFILE");
+			System.exit(1);
+		}
+
+	}
+}
