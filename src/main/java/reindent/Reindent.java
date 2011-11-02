133acd10c4b7009510fb904911ce3f18a2c34da7
Sean Fisk <sean@seanfisk.com>
2011-11-01 23:06:08 -0400

    Properly reformatting the code and print to the console

--- src/main/java/reindent/Reindent.java
+++ src/main/java/reindent/Reindent.java
@@ -18,26 +18,42 @@ public class Reindent
 	 */
 	public static void main(String[] args)
 	{
-		// Argument checking 
+		// Argument checking
 		if(args.length != 2)
 		{
 			System.err.println("Usage: java reindent.Reindent INFILE OUTFILE");
 			System.exit(1);
 		}
-		
-		// Input file checking 
+
+		// Input file checking
 		File inFile = new File(args[0]);
-		if(!inFile.isFile() || !inFile.canRead() || inFile.length() <= 0) 
+		if(!inFile.isFile() || !inFile.canRead() || inFile.length() <= 0)
 		{
-			System.err.println( "reindent: " + inFile.getName() + ": Must be a readable file with content");
+			System.err.println("reindent: " + inFile.getName() + ": Must be a readable file with content");
 			System.exit(1);
 		}
-		
+
 		// Setup scanner
 		Scanner inScanner = null;
 		try
 		{
 			inScanner = new Scanner(inFile);
+			
+			// Read file line by line
+			String line;
+			while(inScanner.hasNextLine())
+			{
+				line = inScanner.nextLine();
+				int numSpaces = 0;
+				while(numSpaces < line.length() && line.charAt(numSpaces) == ' ')
+					++numSpaces;
+				String formattedLine = "";
+				int numTabs = numSpaces / 2;
+				for(int i = 0; i < numTabs; ++i)
+					formattedLine += '\t';
+				formattedLine += line.substring(numSpaces);
+				System.out.println(formattedLine);
+			}
 		}
 		catch(FileNotFoundException e)
 		{
@@ -49,14 +65,5 @@ public class Reindent
 			if(inScanner != null)
 				inScanner.close();
 		}
-		
-		// Read file line by line
-		String line;
-		while(inScanner.hasNextLine())
-		{
-			line = inScanner.nextLine();
-			System.out.println(line);
-		}
-		
 	}
 }
